590aea059352eb9325ab9a4596fbed9d41d1dccc
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-05-20 09:59:50 +0300

    Add TSDoc for mergeObjects() from object.utils

--- projects/ang-toggler/src/lib/utils/object.utils.ts
+++ projects/ang-toggler/src/lib/utils/object.utils.ts
@@ -1,3 +1,25 @@
+/**
+ * Merge objects fields
+ *
+ * @param target - The object into which other fields will be merged
+ * @param supplierOrSuppliers - The object or list of objects which fields will be merged to the target.
+ * If it is an array the order matters, so first objects fields have a precedence over subsequent.
+ *
+ * @example
+ * Example with 1 object passed as supplier
+ *
+ * // returns { a: 'a1', b: 'b2', c: 'c2' }
+ * mergeObjects({ a: 'a1', b: 'b1' }, { b: 'b2', c: 'c3' });
+ *
+ * @example
+ * Example with an objects list passed as suppliers
+ *
+ * // returns { a: 'a1', b: 'b2', c: 'c2', d: 'd3' }
+ * mergeObjects({ a: 'a1', b: 'b1', c: 'c1' }, [{ b: 'b2', c: 'c2' }, { c: 'c3', d: 'd3' }]);
+ *
+ * @returns a new object with contains all fields from target, fields match with suppliers will be overwritten,
+ * other fields embraces the target.
+ **/
 export const mergeObjects = (
   target: Record<string, any>,
   supplierOrSuppliers: Record<string, any>[] | Record<string, any>
